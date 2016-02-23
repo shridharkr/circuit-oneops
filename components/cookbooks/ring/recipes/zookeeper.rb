ec4528b03fb83246f4fdaca4706ad249f6cfb370
Suman Mittapally - smittap <smittap@L-SB8W3DJFD5-M.local>
2016-02-22 21:20:03 -0800

    DNS fix

--- components/cookbooks/ring/recipes/zookeeper.rb
+++ components/cookbooks/ring/recipes/zookeeper.rb
@@ -4,9 +4,9 @@ nodes = node.workorder.payLoad.ManagedVia
 dns_record = ""
 nodes.each do |n|
   if dns_record == ''
-    dns_record = n[:ciAttributes][:dns_record]
+    dns_record = n[:ciAttributes][:private_ip]
   else
-    dns_record += ',' + n[:ciAttributes][:dns_record]
+    dns_record += ',' + n[:ciAttributes][:private_ip]
   end
 end
 puts "***RESULT:dns_record=#{dns_record}"
