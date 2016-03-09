63668c5c05cbd1790c54981eecca5c44f878ebbe
samawia-moin-confiz <samawiamoin@confiz.com>
2016-03-09 12:45:43 +0500

    Checked 100% code coverage

--- components/cookbooks/azuredns/spec/remove_old_aliases_spec.rb
+++ components/cookbooks/azuredns/spec/remove_old_aliases_spec.rb
@@ -4,12 +4,9 @@ require 'json'
 # rubocop:disable LineLength
 # rubocop:disable StringLiterals
 # rubocop:disable WordArray
-require 'simplecov'
-SimpleCov.start
 require File.expand_path('../../libraries/dns.rb', __FILE__)
 require File.expand_path('../../libraries/record_set.rb', __FILE__)
 describe AzureDns::DNS do
-
   describe '#validate_customer_domain' do
     file_path = File.expand_path('test_json_data.json', __dir__)
     file = File.open(file_path)
@@ -92,7 +89,7 @@ describe AzureDns::DNS do
     end
     it 'throws exception when cloud_dns_id is empty' do
       service_attrs['cloud_dns_id'] = nil
-      expect(dns_obj.checking_cloud_dns_id(service_attrs, cloud_service)).to raise_exception(Exception)
+      expect { dns_obj.checking_cloud_dns_id(service_attrs, cloud_service) }.to raise_exception(Exception)
     end
   end
 
@@ -126,10 +123,9 @@ describe AzureDns::DNS do
     dns_obj = AzureDns::DNS.new(service_attrs, token, resource_group)
 
     context 'when hostname entry is false' do
-      it ' has an emtry array of aliases' do
-        node_attr['workorder']['rfcCi']['ciBaseAttributes']['aliases'] = "[\"\"]"
-        aliases = dns_obj.get_aliases(node_attr['workorder']['rfcCi'], false)
-        expect(aliases).to eq([""])
+      it ' rescue JSON Parse error' do
+        node_attr['workorder']['rfcCi']['ciBaseAttributes']['aliases'] = "[\"\"\"]"
+        expect { dns_obj.get_aliases(node_attr['workorder']['rfcCi'], false) }.to_not raise_error
       end
     end
     it 'gives nil when hostname entry is true' do
@@ -159,10 +155,9 @@ describe AzureDns::DNS do
         expect(current_aliases).to eq(["alias2"])
       end
 
-      it ' has an emtry array of current aliases' do
-        node_attr['workorder']['rfcCi']['ciAttributes']['aliases'] = "[\"\"]"
-        current_aliases = dns_obj.get_current_aliases(node_attr['workorder']['rfcCi'], false)
-        expect(current_aliases).to eq([""])
+      it ' rescue JSON Parse error' do
+        node_attr['workorder']['rfcCi']['ciAttributes']['aliases'] = "[\"\"\"]"
+        expect { dns_obj.get_current_aliases(node_attr['workorder']['rfcCi'], false) }.to_not raise_error
       end
     end
   end
@@ -222,10 +217,9 @@ describe AzureDns::DNS do
       full_aliases = dns_obj.get_full_aliases(node_attr['workorder']['rfcCi'], true)
       expect(full_aliases).to be_nil
     end
-    it ' has an emtry array of full aliases' do
-      node_attr['workorder']['rfcCi']['ciBaseAttributes']['full_aliases'] = "[\"\"]"
-      aliases = dns_obj.get_full_aliases(node_attr['workorder']['rfcCi'], false)
-      expect(aliases).to eq([""])
+    it ' rescue JSON Parse error' do
+      node_attr['workorder']['rfcCi']['ciBaseAttributes']['full_aliases'] = "[\"\"\"]"
+      expect { dns_obj.get_full_aliases(node_attr['workorder']['rfcCi'], false) }.to_not raise_error
     end
   end
 
@@ -244,10 +238,9 @@ describe AzureDns::DNS do
         current_full_aliases = dns_obj.get_current_full_aliases(node_attr['workorder']['rfcCi'], false)
         expect(current_full_aliases).to eq(["full-alias2"])
       end
-      it ' has an emtry array of current full aliases' do
-        node_attr['workorder']['rfcCi']['ciAttributes']['full_aliases'] = "[\"\"]"
-        current_full_aliases = dns_obj.get_current_full_aliases(node_attr['workorder']['rfcCi'], false)
-        expect(current_full_aliases).to eq([""])
+      it ' rescue JSON Parse error' do
+        node_attr['workorder']['rfcCi']['ciAttributes']['full_aliases'] = "[\"\"\"]"
+        expect { dns_obj.get_current_full_aliases(node_attr['workorder']['rfcCi'], false) }.to_not raise_error
       end
     end
     context 'when hostname entry is true' do
@@ -305,28 +298,43 @@ describe AzureDns::DNS do
     entries_response = ['contoso.com']
     entries_nil = nil
 
-    priority = node_attr['workorder']['cloud']['ciAttributes']['priority']
     context 'when aliases are not nil' do
-      it 'sets deletable entries on the basis of aliases with priority 0 & 1' do
+      it 'sets deletable entries on the basis of aliases with priority 0' do
+        priority = node_attr['workorder']['cloud']['ciAttributes']['priority']
         dns_obj = AzureDns::DNS.new(service_attrs, token, resource_group)
         allow(dns_obj.recordset).to receive(:get_existing_records_for_recordset) { entries_response }
         dns_obj.get_aliases(node_attr['workorder']['rfcCi'], false)
         entries = dns_obj.set_alias_entries_to_be_deleted('.env.asm.org', priority, service_attrs['cloud_dns_id'])
         expect(entries).to eq([{ name: "alias1.env.asm.org", values: "contoso.com" }, { name: "alias2.env.asm.org", values: "contoso.com" }])
       end
+      it 'sets deletable entries on the basis of aliases with priority 1' do
+        priority = node_attr['workorder']['cloud']['ciAttributes']['priority'] = "1"
+        dns_obj = AzureDns::DNS.new(service_attrs, token, resource_group)
+        allow(dns_obj.recordset).to receive(:get_existing_records_for_recordset) { entries_response }
+        dns_obj.get_aliases(node_attr['workorder']['rfcCi'], false)
+        entries = dns_obj.set_alias_entries_to_be_deleted('.env.asm.org', priority, service_attrs['cloud_dns_id'])
+        expect(entries).to eq([{ name: "alias1.env.asm.org", values: "contoso.com" },
+                               { name: "alias1.env", values: "contoso.com" },
+                               { name: "alias2.env.asm.org", values: "contoso.com" },
+                               { name: "alias2.env", values: "contoso.com" }])
+      end
     end
     context 'when aliases and value is nil' do
-      it 'throws an exception when aliases are nil' do
+      it 'returns emty entries array when value is nil' do
+        priority = node_attr['workorder']['cloud']['ciAttributes']['priority']
+        dns_obj = AzureDns::DNS.new(service_attrs, token, resource_group)
+        allow(dns_obj.recordset).to receive(:get_existing_records_for_recordset) { entries_nil }
+        dns_obj.get_aliases(node_attr['workorder']['rfcCi'], false)
+        entries = dns_obj.set_alias_entries_to_be_deleted('.env.asm.org', priority, service_attrs['cloud_dns_id'])
+        expect(entries).to eq([])
+      end
+
+      it 'returns emty entries array when aliases are nil' do
+        priority = node_attr['workorder']['cloud']['ciAttributes']['priority']
         dns_obj = AzureDns::DNS.new(service_attrs, token, resource_group)
         node_attr['workorder']['rfcCi']['ciBaseAttributes']['aliases'] = "[\"\"]"
         dns_obj.get_aliases(node_attr['workorder']['rfcCi'], false)
         expect(dns_obj.set_alias_entries_to_be_deleted('.env.asm.org', priority, service_attrs['cloud_dns_id'])).to eq([])
-      end
-      it 'throws an exception when value is nil because entries will be empty' do
-        dns_obj = AzureDns::DNS.new(service_attrs, token, resource_group)
-        allow(dns_obj).to receive(:set_alias_entries_to_be_deleted) { entries_nil }
-        entries = dns_obj.set_alias_entries_to_be_deleted('.env.asm.org', priority, service_attrs['cloud_dns_id'])
-        expect(entries).to be_nil
       end
     end
   end
@@ -359,6 +367,29 @@ describe AzureDns::DNS do
         dns_obj.get_full_aliases(node_attr['workorder']['rfcCi'], false)
         expect(dns_obj.set_full_alias_entries_to_be_deleted).to eq([])
       end
+    end
+  end
+
+  describe '#functions_on_entries' do
+    file_path = File.expand_path('test_json_data.json', __dir__)
+    file = File.open(file_path)
+    contents = file.read
+    node_attr = JSON.parse(contents)
+    cloud_service = node_attr['workorder']['services']['dns']['azure']
+    service_attrs = cloud_service['ciAttributes']
+    token = node_attr['azure_rest_token']
+    resource_group = node_attr['platform-resource-group']
+
+    it 'calls the functions and return result' do
+      responsefromremoverecordset = ''
+      entries_response = ['contoso.com']
+      dns_obj = AzureDns::DNS.new(service_attrs, token, resource_group)
+      allow(dns_obj.recordset).to receive(:get_existing_records_for_recordset) { entries_response }
+      allow(dns_obj.recordset).to receive(:remove_record_set) { responsefromremoverecordset }
+      dns_obj.functions_on_aliases_and_fullaliases(node_attr['workorder']['rfcCi'], false)
+      priority = node_attr['workorder']['cloud']['ciAttributes']['priority']
+      entries_result = dns_obj.functions_on_entries('.env.asm.org', priority, service_attrs['cloud_dns_id'])
+      expect(entries_result).to eq([{ name: "alias1.env.asm.org", values: "contoso.com" }, { name: "full-alias1", values: "contoso.com" }])
     end
   end
 
