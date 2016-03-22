fce77282c3c44461e01bb94b52886865691afed5
Kevin Griffith <kpgriff@L-HOAP3Z3G3Q-M.homeoffice.wal-mart.com>
2016-03-22 16:26:19 -0500

    refactor add availability set recipe to class

--- components/cookbooks/azure/recipes/add_keypair.rb
+++ components/cookbooks/azure/recipes/add_keypair.rb
@@ -1,12 +1,26 @@
 require 'azure_mgmt_resources'
 
+cloud_name = node[:workorder][:cloud][:ciName]
+compute_service =
+  node[:workorder][:services][:compute][cloud_name][:ciAttributes]
+
 # Setup Azure credentials
+##  TODO: need to remove this when resource group is refactored.
 include_recipe 'azure::get_credentials'
 
 # Create Resource Group
 include_recipe 'azure::add_resource_group'
 
+# get the availability set to use
+availability_set = AzureCompute::AvailabilitySet.new(compute_service)
 # Create Availability Set
-include_recipe 'azure::add_availability_set'
+availability_set.add(node['platform-resource-group'],
+                     node['platform-availability-set'],
+                     compute_service['location'])
 
-Chef::Log.info("Exiting add keypair")
+Chef::Log.info('Exiting add keypair')
+
+puts("***FAULT:FATAL=FAIL FOR TESTING")
+ex = Exception.new('no backtrace')
+ex.set_backtrace('')
+raise ex
