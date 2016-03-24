ed677e06a931c863fba294a72b90e9fb9a879c00
Kevin Griffith <kpgriff@L-HOAP3Z3G3Q-M.local>
2016-03-24 10:49:41 -0500

    adding resource group class

--- components/cookbooks/azure/libraries/resource_group.rb
+++ components/cookbooks/azure/libraries/resource_group.rb
@@ -2,14 +2,15 @@
 module AzureResources
   # class to handle operations on the Azure Resource Group
   class ResourceGroup
-
     def initialize(compute_service)
-      creds = AzureCommon::AzureUtils.get_credentials(compute_service['tenant_id'], compute_service['client_id'], compute_service['client_secret'])
+      creds =
+        AzureCommon::AzureUtils.get_credentials(compute_service['tenant_id'],
+                                                compute_service['client_id'],
+                                                compute_service['client_secret']
+                                               )
 
       @client = Azure::ARM::Compute::ComputeManagementClient.new(creds)
       @client.subscription_id = compute_service['subscription']
     end
-
-
   end
 end
