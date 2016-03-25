311d7ae29d2c0c1adf93f785132e48345045a28b
Suman Mittapally - smittap <smittap@L-SB8W3DJFD5-M.local>
2016-03-25 16:43:42 -0700

    Updated status recipe

--- components/cookbooks/jboss/recipes/status.rb
+++ components/cookbooks/jboss/recipes/status.rb
@@ -2,4 +2,7 @@
 # Cookbook Name:: jboss
 # Recipe:: status
 #
-Chef::Log.info(`service jboss status`)
+cmd = Mixlib::ShellOut.new("service jboss status")
+cmd.run_command
+cmd.stdout
+Chef::Log.info("Execution completed\n#{cmd.format_for_exception}")
