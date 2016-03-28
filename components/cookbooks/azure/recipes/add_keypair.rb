9784891edc2d96f6599d753483660e8d54bedb7f
Kevin Griffith <kpgriff@L-HOAP3Z3G3Q-M.local>
2016-03-28 07:53:48 -0500

    updating to use the proxy

--- components/cookbooks/azure/recipes/add_keypair.rb
+++ components/cookbooks/azure/recipes/add_keypair.rb
@@ -1,4 +1,10 @@
 require 'azure_mgmt_resources'
+require File.expand_path('../../libraries/azure_utils.rb', __FILE__)
+
+::Chef::Recipe.send(:include, AzureCommon)
+
+#set the proxy if it exists as a cloud var
+AzureCommon::AzureUtils.set_proxy(node.workorder.payLoad.OO_CLOUD_VARS)
 
 # Setup Azure credentials
 include_recipe 'azure::get_credentials'
