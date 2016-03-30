82fae2b1d7d3f8e229e3e90d0f949357c1063b30
Imaad Ghouri <ighouri@walmartlabs.com>
2016-03-30 12:12:15 -0700

    gracefully exiting out of the recipe without chef failure for rackspace actions that are not currently supported

--- components/cookbooks/lb/recipes/unbind_cloud.rb
+++ components/cookbooks/lb/recipes/unbind_cloud.rb
@@ -34,7 +34,7 @@ end
 
 if cloud_service[:ciClassName] !~ /netscaler/i
   Chef::Log.info("lb cloud service is #{cloud_service} but this action is only supported for netscaler cloud service hence exiting...")
-  exit 0
+  return
 end
 
 #include_recipe "lb::get_lb_name"
