90fbfa8329e6091935a501af4135b6fe1074370a
mike schwankl <mschwan@walmart.com>
2016-04-01 11:41:22 -0600

    make gdns optional

--- packs/base.rb
+++ packs/base.rb
@@ -186,7 +186,7 @@ resource 'logstash',
 resource "fqdn",
   :cookbook => "oneops.1.fqdn",
   :design => true,
-  :requires => { "constraint" => "1..1", "services" => "compute,dns,gdns" },
+  :requires => { "constraint" => "1..1", "services" => "compute,dns,*gdns" },
   :attributes => { "aliases" => '[]' },
   :payloads => {
 'environment' => {
