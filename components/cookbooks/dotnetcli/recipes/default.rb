2f4ff0dd54e0698f36b91d278eb801e13e268ea0
Murali Krishna <mkrish6@L-HOAFEZAG8W-M.homeoffice.wal-mart.com>
2016-04-04 16:05:49 -0500

    first commit for dotnetcli

---
+++ components/cookbooks/dotnetcli/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: dotnetcli
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+default[:dotnetcli][:example_file_name] = '/tmp/helloworld3.txt'
