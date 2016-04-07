5fcf39fe83c424ddb42815077fcf6cb41e729b67
haider-ali-confiz <haider.ali@confiz.com>
2016-04-07 17:55:19 +0500

    Modified Virtual network library.Added create_update_network method in it

--- components/cookbooks/azure/libraries/virtual_network.rb
+++ components/cookbooks/azure/libraries/virtual_network.rb
@@ -1,43 +1,97 @@
 module AzureNetwork
-  class AzureVirtualNetwork
+
+  class VirtualNetwork
 
     def initialize(credentials, subscription_id)
       @client = Azure::ARM::Network::NetworkResourceProviderClient.new(credentials)
       @client.subscription_id = subscription_id
     end
 
-    def create_update_network(resource_group_name, network_name, virtual_network)
+    def create_update_network(resource_group_name, vnet_name, virtual_network)
       begin
 
-        promise = @client.virtual_networks.create_or_update(resource_group_name, network_name, virtual_network)
+        puts("Creating Virtual Network '#{vnet_name}' ...")
+        start_time = Time.now.to_i
+        promise = @client.virtual_networks.create_or_update(resource_group_name, vnet_name, virtual_network)
         result = promise.value!
         virtual_network_obj = result.body
-        Chef::Log.info('Successfully created/updated network name: ' + network_name)
+        end_time = Time.now.to_i
+        duration = end_time - start_time
+        Chef::Log.info('Successfully created/updated network name: ' + vnet_name)
+        puts("operation took #{duration} seconds")
         return virtual_network_obj
 
       rescue MsRestAzure::AzureOperationError => e
-        Chef::Log.error('***FAULT:FATAL=creating/updating ' + network_name + ' in resource group: ' + resource_group_name)
+        Chef::Log.error('***FAULT:FATAL=creating/updating ' + vnet_name + ' in resource group: ' + resource_group_name)
         Chef::Log.error('***FAULT:FATAL=' + e.body.to_s)
         exit 1
       end
 
     end
 
-    def get_vnet(resource_group_name, network_name)
+    def get_vnet(resource_group_name, vnet_name)
       begin
-        promise = @client.virtual_networks.get(resource_group_name, network_name)
+        puts("Getting Virtual Network '#{vnet_name}' ...")
+        start_time = Time.now.to_i
+        promise = @client.virtual_networks.get(resource_group_name, vnet_name)
         response = promise.value!
         result = response.body
+        end_time = Time.now.to_i
+        duration = end_time - start_time
+
+        puts("operation took #{duration} seconds")
+
         return result
-      rescue MsRestAzure::AzureOperationError => e
-        Chef::Log.error('Network ' + network_name + ' not found')
-        Chef::Log.error('Error: ' + e.body.to_s)
-        return nil
+      rescue  MsRestAzure::AzureOperationError =>e
+        puts 'Error creating Virtual Network'
+        puts("Error Response: #{e.response}")
+        puts("Error Body: #{e.body}")
       end
     end
-  end
+
+    def get_vnets(resource_group_name)
+      begin
+        puts("Getting vnets from Resource Group '#{resource_group_name}' ...")
+        start_time = Time.now.to_i
+        promise = @client.virtual_networks.list(resource_group_name)
+        response = promise.value!
+        result = response.body
+        end_time = Time.now.to_i
+        duration = end_time - start_time
+
+        puts("operation took #{duration} seconds")
+
+        return result
+
+      rescue  MsRestAzure::AzureOperationError =>e
+        puts 'Error creating Virtual Network'
+        puts("Error Response: #{e.response}")
+        puts("Error Body: #{e.body}")
+      end
+    end
+
+    def get_subscription_vnets
+      begin
+
+        puts("Getting subscription vnets ...")
+        start_time = Time.now.to_i
+        promise = @client.virtual_networks.list_all()
+        response = promise.value!
+        result = response.body
+        end_time = Time.now.to_i
+        duration = end_time - start_time
+
+        puts("operation took #{duration} seconds")
+
+        return result
+
+      rescue  MsRestAzure::AzureOperationError =>e
+        puts 'Error creating Virtual Network'
+        puts("Error Response: #{e.response}")
+        puts("Error Body: #{e.body}")
+      end
+    end
+
+  end  # end of class
+
 end
-
-
-
-
