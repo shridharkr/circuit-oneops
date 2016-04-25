8fe824e0a384ef99044a34df68ecdc0dfb69ccae
Adeeti Kaushal <akaush1@L-SB8T2NAFD5-M.local>
2016-04-25 15:44:50 -0700

    Changes related to Password Encryption

--- components/cookbooks/activemq/recipes/repair.rb
+++ components/cookbooks/activemq/recipes/repair.rb
@@ -1,8 +1,4 @@
 # Cookbook Name:: activemq
 # Recipe:: repair
 #
-
-service "activemq" do
-  supports  :restart => true, :status => true, :stop => true, :start => true
-  action :restart
-end
+include_recipe "activemq::restart"
