d0ee705a2b4aaefbce077ad7bc607ed525855532
Ravinder Matte <ravinder.matte@gmail.com>
2016-05-04 11:21:37 -0700

    wip

--- components/cookbooks/cassandra/recipes/config_directives.rb
+++ components/cookbooks/cassandra/recipes/config_directives.rb
@@ -33,6 +33,15 @@ ruby_block 'update_config_directives' do
     cfg["seed_provider"]=[{"class_name"=>"org.apache.cassandra.locator.SimpleSeedProvider", "parameters"=>[{"seeds"=>node[:initial_seeds].join(",")}]}]
     puts "full cfg: #{cfg.to_yaml}"
 
+    cfg["listen_address"] = node.workorder.payLoad.ManagedVia[0][:ciAttributes][:private_ip]
+    cfg["broadcast_rpc_address"] = node.workorder.payLoad.ManagedVia[0][:ciAttributes][:private_ip]
+    cfg["rpc_address"] = "0.0.0.0"
+
+    if node[:workorder][:rfcCi][:ciAttributes].has_key?("endpoint_snitch")
+      cfg["endpoint_snitch"] = node.workorder.rfcCi[:ciAttributes][:endpoint_snitch]
+    end
+
+
     yaml_file = '/opt/cassandra/conf/cassandra.yaml'
     Chef::Application.fatal!("Can't find the YAML config file - #{yaml_file} ") if !File.exists? yaml_file
     merge_conf_directives(yaml_file, cfg)
