ecfbfe4044669f3a7c67697b180543756f836793
Rene Saenz <edgar.saenz@walmart.com>
2016-05-05 09:44:08 -0500

    Initial Branch

--- components/cookbooks/azure/recipes/reboot_node.rb
+++ components/cookbooks/azure/recipes/reboot_node.rb
@@ -1,35 +1,40 @@
-require 'json'
 require 'azure_mgmt_compute'
+require 'json'
+require File.expand_path('../../libraries/azure_utils.rb', __FILE__)
+
+::Chef::Recipe.send(:include, Azure::ARM::Compute)
+::Chef::Recipe.send(:include, Azure::ARM::Compute::Models)
 
 #set the proxy if it exists as a system prop
 AzureCommon::AzureUtils.set_proxy_from_env(node)
 
-cloud_name = node[:workorder][:cloud][:ciName]
-compute_service = node[:workorder][:services][:compute][cloud_name][:ciAttributes]
-credentials = AzureCommon::AzureUtils.get_credentials(compute_service[:tenant_id],
-                                                      compute_service[:client_id],
-                                                      compute_service[:client_secret])
-location = compute_service[:location]
-subscription_id = compute_service[:subscription]
+include_recipe "azure::get_platform_rg_and_as"
+include_recipe "azure::get_credentials"
+include_recipe "azure::status_node"
 
-ci = node[:workorder][:ci]
-vm_name = ci[:ciAttributes][:instance_name]
-node.set['vm_name'] = vm_name
-metadata = ci[:ciAttributes][:metadata]
-metadata_obj= JSON.parse(metadata)
-org = metadata_obj['organization']
-assembly = metadata_obj['assembly']
-environment = metadata_obj['environment']
-# platform = metadata_obj['platform']
-# platform_name = node.workorder.box.ciName
-platform_ciID = node.workorder.box.ciId
-
-
-resource_group_name = AzureResources::ResourceGroup.get_name(org, assembly, platform_ciID, environment, location)
-begin
-  vm_svc = AzureCompute::VirtualMachine.new(credentials, subscription_id)
-  vm_svc.restart(resource_group_name, vm_name)
-  node.set['reboot_result'] = 'Success'
-rescue Exception => e
-  node.set['reboot_result'] = 'Error'
+if node["status_result"] == "Success"
+  begin
+    client = ComputeManagementClient.new(node['azureCredentials'])
+    client.subscription_id = node['subscriptionid']
+    Chef::Log.info("reboot in progress")
+    start_time = Time.now.to_i
+    promise = client.virtual_machines.restart(node['platform-resource-group'],node['vm_name'])
+    result = promise.value!
+    end_time = Time.now.to_i
+    duration = end_time - start_time
+    Chef::Log.error("VM rebooted in #{duration} seconds")
+    node.set["reboot_result"]="Success"
+  rescue  MsRestAzure::AzureOperationError => e
+    node.set["reboot_result"]="Error"
+    Chef::Log.error("Error rebooting VM #{vm_name}")
+    if e.body != nil
+      error_response = e.body["error"]
+      Chef::Log.error("Error Response code:" +error_response["code"])
+      Chef::Log.error("Error Response message:" +error_response["message"])
+    else
+      Chef::Log.error("Error:"+e.inspect)
+    end
+  end
+else
+  node.set["reboot_result"]="Error"
 end
--- components/cookbooks/azure/recipes/status_node.rb
+++ components/cookbooks/azure/recipes/status_node.rb
@@ -1,38 +1,32 @@
-require 'json'
 require 'azure_mgmt_compute'
+require 'json'
+require File.expand_path('../../libraries/azure_utils.rb', __FILE__)
+
+::Chef::Recipe.send(:include, Azure::ARM::Compute)
+::Chef::Recipe.send(:include, Azure::ARM::Compute::Models)
 
 #set the proxy if it exists as a system prop
 AzureCommon::AzureUtils.set_proxy_from_env(node)
 
-cloud_name = node[:workorder][:cloud][:ciName]
-compute_service = node[:workorder][:services][:compute][cloud_name][:ciAttributes]
-credentials = AzureCommon::AzureUtils.get_credentials(compute_service[:tenant_id],
-                                                      compute_service[:client_id],
-                                                      compute_service[:client_secret])
-location = compute_service[:location]
-subscription_id = compute_service[:subscription]
+include_recipe 'azure::get_platform_rg_and_as'
+include_recipe "azure::get_credentials"
 
-ci = node[:workorder][:ci]
-vm_name = ci[:ciAttributes][:instance_name]
-node.set['vm_name'] = vm_name
-metadata = ci[:ciAttributes][:metadata]
-metadata_obj= JSON.parse(metadata)
-org = metadata_obj['organization']
-assembly = metadata_obj['assembly']
-environment = metadata_obj['environment']
-platform_ciID = node.workorder.box.ciId
-
-resource_group_name = AzureResources::ResourceGroup.get_name(org, assembly, platform_ciID, environment, location)
 begin
-  vm_svc = AzureCompute::VirtualMachine.new(credentials, subscription_id)
-  vm = vm_svc.get(resource_group_name, vm_name)
-  # OOLog.info("server info: " + vm.server_info.inspect.gsub(/\n|\<|\>|\{|\}/,""))
-  OOLog.info("Status result. Instance Name: [#{vm.name}]")
-  OOLog.info("Status result. Type: [#{vm.type}]")
-  OOLog.info("Status result. Location: [#{vm.location}]")
-  OOLog.info("Status result. Computer Name: [#{vm.properties.os_profile.computer_name}]")
-  node.set['status_result'] = 'Success'
-rescue Exception => e
-  node.set['status_result'] = 'Error'
+  client = ComputeManagementClient.new(node['azureCredentials'])
+  client.subscription_id = node['subscriptionid']
+  promise = client.virtual_machines.get(node['platform-resource-group'],node['vm_name'])
+  result = promise.value!
+  node.set["status_result"]="Success"
+  node.set["server_info"]=result.body
+  Chef::Log.info("server info: " + node.server_info.inspect.gsub(/\n|\<|\>|\{|\}/,""))
+rescue  MsRestAzure::AzureOperationError =>e
+  Chef::Log.error("Error getting VM status for #{vm_name}")
+  node.set["status_result"]="Error"
+  if e.body != nil
+    error_response = e.body["error"]
+    Chef::Log.error("Error Response code:" +error_response["code"])
+    Chef::Log.error("Error Response message:" +error_response["message"])
+  else
+    Chef::Log.error("Error:"+e.inspect)
+  end
 end
-
