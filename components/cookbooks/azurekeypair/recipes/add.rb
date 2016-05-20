083c299b543cf39e686dd4a78dc9781c4ce476d5
Rene Saenz <edgar.saenz@walmart.com>
2016-05-20 16:42:22 -0500

    Improved refactoring of azurekeypair cookbook

--- components/cookbooks/azurekeypair/recipes/add.rb
+++ components/cookbooks/azurekeypair/recipes/add.rb
@@ -1,7 +1,5 @@
 
 #set the proxy if it exists as a cloud var
-# AzureCommon::AzureUtils.set_proxy(node[:workorder][:payLoad][:OO_CLOUD_VARS])
-
 Utils.set_proxy(node[:workorder][:payLoad][:OO_CLOUD_VARS])
 
 # create the resource group
