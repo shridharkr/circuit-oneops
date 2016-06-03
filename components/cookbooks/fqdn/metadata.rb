974ec4fe1192935974027389c02fe550c4a595d2
mike schwankl <mschwan@walmart.com>
2016-06-03 10:21:52 -0600

    fix regexes

--- components/cookbooks/fqdn/metadata.rb
+++ components/cookbooks/fqdn/metadata.rb
@@ -21,7 +21,7 @@ attribute 'aliases',
   :format => {
     :help => 'List of additional short-name aliases to be configured in the DNS service (Note: the FQDN record of these CNAME aliases will include the environment subdomain)',
     :category => '1.Global',
-    :pattern => '[^\s]+[a-zA-Z0-9\-]*[^\s]+',
+    :pattern => '[^-][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]',
     :order => 1
   }
 
@@ -31,7 +31,7 @@ attribute 'full_aliases',
   :format => {
     :help => 'List of additional full-domain name aliases to be configured in the DNS service (Note: the FQDN record of these CNAME aliases will *not* include the environment subdomain)',
     :category => '1.Global',
-    :pattern => '[^\s]+(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])[^\s]+',
+    :pattern => '([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])',
     :order => 2
   }
 
