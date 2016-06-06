00c7dad5c925fbe5ff88260c7436dc79eb771386
Imaad Ghouri <ighouri@walmartlabs.com>
2016-06-06 14:25:11 -0700

    changing chef service resource with shell command as status service action not supported

--- components/cookbooks/logstash/recipes/status.rb
+++ components/cookbooks/logstash/recipes/status.rb
@@ -1,3 +1,8 @@
-service 'logstash' do
-  action :status
-end 
+
+_exec_cmd = "service logstash status"
+ruby_block "#{_exec_cmd}" do
+	block do
+		Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
+		shell_out!("#{_exec_cmd}", :live_stream => Chef::Log::logger)
+    end
+end
