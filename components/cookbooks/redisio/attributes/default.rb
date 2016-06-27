4af8784abcb7eaa805463ccdee71bf3766edb3a0
Ning Zhang <nzhang@walmartlabs.com>
2016-06-26 23:39:16 -0700

    Keep up Redisio cookbook with upstream
    The OneOps redisio cookbook was mostly inherited from
    
    https://github.com/brianbianco/redisio
    
    and the latest version of `brianbianco/redisio` (upstream) is 2.4.2,
    while the OneOps redis cookbook version is only 1.7.0 (June 2013).
    
    This is an effort to sync up with the upstream without losing the
    compatibility with OneOps. For example, the updated code base will
    still seamlessly support Redis `cluster` deployment with version 3.0.1
    
    Note that the upstream cookbook supports `Redis Sentinal`, while the
    OneOps redis cookbook did not. In this patch the sentinal related code
    are checked in (to align with upstream) but the OneOps redis pack still does
    not support Redis Sentinal deployment. In case Redis Sentinal will be
    expected in future, supporting it should not be very complex (as the upstream
    cookbook already provided a skeleton of Redis Sentinal deployment).
    
    Overall, this patch does not eliminate or downgrade any existing
    features and supported case/deployment, but just wants to keep up the
    existing redisio cookbook with upstream. More supported deployment case
    (e.g. redis sentinal) and improvements could be addressed later.
    
    Deployment tests have been done.

--- components/cookbooks/redisio/attributes/default.rb
+++ components/cookbooks/redisio/attributes/default.rb
@@ -1,3 +1,4 @@
+
 # Cookbook Name:: redisio
 # Attribute::default
 #
@@ -17,117 +18,220 @@
 #
 
 case node['platform']
-when 'ubuntu','debian'
-  shell = '/bin/false'
-  homedir = '/var/lib/redis'
-when 'centos','redhat','scientific','amazon','suse'
-  shell = '/bin/sh'
-  homedir = '/var/lib/redis' 
-when 'fedora'
-  shell = '/bin/sh'
-  homedir = '/home' #this is necessary because selinux by default prevents the homedir from being managed in /var/lib/ 
-else
-  shell = '/bin/sh'
-  homedir = '/redis'
+    when 'ubuntu','debian'
+    shell = '/bin/false'
+    homedir = '/var/lib/redis'
+    package_name = 'redis-server'
+    when 'centos','redhat','scientific','amazon','suse'
+    shell = '/bin/sh'
+    homedir = '/var/lib/redis'
+    package_name = 'redis'
+    when 'fedora'
+    shell = '/bin/sh'
+    homedir = '/home' #this is necessary because selinux by default prevents the homedir from being managed in /var/lib/
+    package_name = 'redis'
+    else
+    shell = '/bin/sh'
+    homedir = '/redis'
+    package_name = 'redis'
 end
 
-default['version'] = "3.0.1"
-
-# this one added locally for Tarball and download related defaults
-#default['redisio']['src_url'] = "http://download.redis.io/releases/redis-2.6.16.tar.gz"
-#default['redisio']['src_url'] = "https://github.com/antirez/redis/archive/3.0.0-beta8.tar.gz"
-
-
-
-#Install related attributes
+# Install related attributes
 default['redisio']['safe_install'] = true
-default['redisio']['mirror'] = "$OO_CLOUD{nexus}/nexus/content/repositories/thirdparty/content/redis/io/redis/"
+default['redisio']['package_install'] = false
+default['redisio']['package_name'] = package_name
+default['redisio']['bypass_setup'] = false
+
+# Tarball and download related defaults
+default['redisio']['mirror'] = "http://download.redis.io/releases/"
 default['redisio']['base_name'] = 'redis-'
 default['redisio']['artifact_type'] = 'tar.gz'
-default['redisio']['version'] = '3.0.1'
 default['redisio']['base_piddir'] = '/var/run/redis'
 
-#Custom installation directory
+# Version
+if node['redisio']['package_install']
+    # latest version (only for package install)
+    default['redisio']['version'] = nil
+    else
+    # force version for tarball
+    default['redisio']['version'] = '3.0.1'
+end
+
+# Custom installation directory
 default['redisio']['install_dir'] = nil
 
-#Default settings for all redis instances, these can be overridden on a per server basis in the 'servers' hash
-# this set was in the community cookbook but actually were incompatible out of the box so commented
-#_  'syslogenabled'          => 'yes',
-#_  'syslogfacility'         => 'local0',
-#_  'slaveservestaledata'    => 'yes',
-#_  'replpingslaveperiod'    => '10',
-#_  'repltimeout'            => '60',
-#_  'maxmemorypolicy'        => 'volatile-lru',
-#_  'maxmemorysamples'       => '3',
-#_  'noappendfsynconrewrite' => 'no',
-#_  'aofrewritepercentage'   => '100',
-#_  'aofrewriteminsize'      => '64mb',
-#_  'ulimit'                 => 0,
+# Job control related options (initd, upstart, or systemd)
+if node['platform_family'] == 'rhel' && Gem::Version.new(node['platform_version']) > Gem::Version.new('7.0.0')
+    default['redisio']['job_control'] = 'systemd'
+    else
+    default['redisio']['job_control'] = 'initd'
+end
 
+# Init.d script related options
+default['redisio']['init.d']['required_start'] = []
+default['redisio']['init.d']['required_stop'] = []
+
+# Default settings for all redis instances, these can be overridden on a per server basis in the 'servers' hash
 default['redisio']['default_settings'] = {
-  'user'                   => 'redis',
-  'group'                  => 'redis',
-  'homedir'                => homedir,
-  'shell'                  => shell,
-  'systemuser'             => true,
-  'configdir'              => '/etc/redis',
-  'name'                   => nil,
-  'address'                => nil,
-  'databases'              => '16',
-  'backuptype'             => 'rdb',
-  'datadir'                => '/var/lib/redis',
-  'unixsocket'             => nil,
-  'unixsocketperm'         => nil,
-  'timeout'                => '0',
-  'loglevel'               => 'verbose',
-  'logfile'                => '/log/redis.log',
-  'shutdown_save'          => false,
-  'save'                   => nil, # Defaults to ['900 1','300 10','60 10000'] inside of template.  Needed due to lack of hash subtraction
-  'slaveof'                => nil,
-  'job_control'            => 'initd', 
-  'masterauth'             => nil,
-  'requirepass'            => nil,
-  'maxclients'             => 10000,
-  'maxmemory'              => nil,
-  'appendfsync'            => 'everysec',
-  'cluster-enabled'        => 'no',
-  'cluster-config-file'    => nil, # Defaults to redis instance name inside of template if cluster is enabled.
-  'cluster-node-timeout'   => 5,
-  'includes'               => nil
+    'user'                    => 'redis',
+    'group'                   => 'redis',
+    'homedir'                 => homedir,
+    'shell'                   => shell,
+    'systemuser'              => true,
+    'uid'                     => nil,
+    #    'ulimit'                  => 0,
+    'configdir'               => '/etc/redis',
+    'name'                    => nil,
+    'tcpbacklog'              => '511',
+    'address'                 => nil,
+    'databases'               => '16',
+    'backuptype'              => 'rdb',
+    'datadir'                 => '/var/lib/redis',
+    'unixsocket'              => nil,
+    'unixsocketperm'          => nil,
+    'timeout'                 => '0',
+    'keepalive'               => '0',
+    'loglevel'                => 'notice',
+    'logfile'                 => nil,
+    'syslogenabled'           => 'yes',
+    'syslogfacility'          => 'local0',
+    'shutdown_save'           => false,
+    'save'                    => nil, # Defaults to ['900 1','300 10','60 10000'] inside of template.  Needed due to lack of hash subtraction
+    'stopwritesonbgsaveerror' => 'yes',
+    'rdbcompression'          => 'yes',
+    'rdbchecksum'             => 'yes',
+    'dbfilename'              => nil,
+    'slaveof'                 => nil,
+    'masterauth'              => nil,
+    'slaveservestaledata'     => 'yes',
+    'slavereadonly'           => 'yes',
+    'replpingslaveperiod'     => '10',
+    'repltimeout'             => '60',
+    'repldisabletcpnodelay'   => 'no',
+    'slavepriority'           => '100',
+    'requirepass'             => nil,
+    'rename_commands'         => nil,
+    'maxclients'              => 10000,
+    'maxmemory'               => nil,
+    'maxmemorypolicy'         => nil,
+    'maxmemorysamples'        => nil,
+    'appendfilename'          => nil,
+    'appendfsync'             => 'everysec',
+    'noappendfsynconrewrite'  => 'no',
+    'aofrewritepercentage'    => '100',
+    'aofrewriteminsize'       => '64mb',
+    'luatimelimit'            => '5000',
+    'slowloglogslowerthan'    => '10000',
+    'slowlogmaxlen'           => '1024',
+    'notifykeyspaceevents'    => '',
+    'hashmaxziplistentries'   => '512',
+    'hashmaxziplistvalue'     => '64',
+    'listmaxziplistentries'   => '512',
+    'listmaxziplistvalue'     => '64',
+    'setmaxintsetentries'     => '512',
+    'zsetmaxziplistentries'   => '128',
+    'zsetmaxziplistvalue'     => '64',
+    'hllsparsemaxbytes'       => '3000',
+    'activerehasing'          => 'yes',
+    'clientoutputbufferlimit' => [
+    %w(normal 0 0 0),
+    %w(slave 256mb 64mb 60),
+    %w(pubsub 32mb 8mb 60)
+    ],
+    'hz'                         => '10',
+    'aofrewriteincrementalfsync' => 'yes',
+    'clusterenabled'            => 'no',
+    'clusterconfigfile'        => nil, # Defaults to redis instance name inside of template if cluster is enabled.
+    'clusternodetimeout'       => 5000,
+    'includes'                 => nil,
+    'data_bag_name'            => nil,
+    'data_bag_item'            => nil,
+    'data_bag_key'             => nil
 }
 
 default['redisio']['cluster_settings'] = {
-  'user'                   => 'redis',
-  'group'                  => 'redis',
-  'homedir'                => homedir,
-  'shell'                  => shell,
-  'systemuser'             => true,
-  'configdir'              => '/etc/redis',
-  'name'                   => nil,
-  'address'                => nil,
-  'databases'              => '16',
-  'backuptype'             => 'rdb',
-  'datadir'                => '/var/lib/redis',
-  'unixsocket'             => nil,
-  'unixsocketperm'         => nil,
-  'timeout'                => '0',
-  'loglevel'               => 'verbose',
-  'logfile'                => '/log/redis.log',
-  'shutdown_save'          => false,
-  'save'                   => nil, # Defaults to ['900 1','300 10','60 10000'] inside of template.  Needed due to lack of hash subtraction
-  'slaveof'                => nil,
-  'job_control'            => 'initd', 
-  'masterauth'             => nil,
-  'requirepass'            => nil,
-  'maxclients'             => 10000,
-  'maxmemory'              => nil,
-  'appendfsync'            => 'everysec',
-  'cluster-enabled'        => 'yes',
-  'cluster-config-file'    => nil, # Defaults to redis instance name inside of template if cluster is enabled.
-  'cluster-node-timeout'   => 5,
-  'includes'               => nil
+    'user'                    => 'redis',
+    'group'                   => 'redis',
+    'homedir'                 => homedir,
+    'shell'                   => shell,
+    'systemuser'              => true,
+    'uid'                     => nil,
+    #    'ulimit'                  => 0,
+    'configdir'               => '/etc/redis',
+    'name'                    => nil,
+    'tcpbacklog'              => '511',
+    'address'                 => nil,
+    'databases'               => '16',
+    'backuptype'              => 'rdb',
+    'datadir'                 => '/var/lib/redis',
+    'unixsocket'              => nil,
+    'unixsocketperm'          => nil,
+    'timeout'                 => '0',
+    'keepalive'               => '0',
+    'loglevel'                => 'notice',
+    'logfile'                 => nil,
+    'syslogenabled'           => 'yes',
+    'syslogfacility'          => 'local0',
+    'shutdown_save'           => false,
+    'save'                    => nil, # Defaults to ['900 1','300 10','60 10000'] inside of template.  Needed due to lack of hash subtraction
+    'stopwritesonbgsaveerror' => 'yes',
+    'rdbcompression'          => 'yes',
+    'rdbchecksum'             => 'yes',
+    'dbfilename'              => nil,
+    'slaveof'                 => nil,
+    'masterauth'              => nil,
+    'slaveservestaledata'     => 'yes',
+    'slavereadonly'           => 'yes',
+    'replpingslaveperiod'     => '10',
+    'repltimeout'             => '60',
+    'repldisabletcpnodelay'   => 'no',
+    'slavepriority'           => '100',
+    'requirepass'             => nil,
+    'rename_commands'         => nil,
+    'maxclients'              => 10000,
+    'maxmemory'               => nil,
+    'maxmemorypolicy'         => nil,
+    'maxmemorysamples'        => nil,
+    'appendfilename'          => nil,
+    'appendfsync'             => 'everysec',
+    'noappendfsynconrewrite'  => 'no',
+    'aofrewritepercentage'    => '100',
+    'aofrewriteminsize'       => '64mb',
+    'luatimelimit'            => '5000',
+    'slowloglogslowerthan'    => '10000',
+    'slowlogmaxlen'           => '1024',
+    'notifykeyspaceevents'    => '',
+    'hashmaxziplistentries'   => '512',
+    'hashmaxziplistvalue'     => '64',
+    'listmaxziplistentries'   => '512',
+    'listmaxziplistvalue'     => '64',
+    'setmaxintsetentries'     => '512',
+    'zsetmaxziplistentries'   => '128',
+    'zsetmaxziplistvalue'     => '64',
+    'hllsparsemaxbytes'       => '3000',
+    'activerehasing'          => 'yes',
+    'clientoutputbufferlimit' => [
+    %w(normal 0 0 0),
+    %w(slave 256mb 64mb 60),
+    %w(pubsub 32mb 8mb 60)
+    ],
+    'hz'                         => '10',
+    'aofrewriteincrementalfsync' => 'yes',
+    'clusterenabled'            => 'yes',
+    'clusterconfigfile'        => nil, # Defaults to redis instance name inside of template if cluster is enabled.
+    'clusternodetimeout'       => 5000,
+    'includes'                 => nil,
+    'data_bag_name'            => nil,
+    'data_bag_item'            => nil,
+    'data_bag_key'             => nil
 }
 
 # The default for this is set inside of the "install" recipe. This is due to the way deep merge handles arrays
 default['redisio']['servers'] = nil
 
+# Define binary path
+if node['redisio']['package_install']
+    default['redisio']['bin_path'] = '/usr/bin'
+    else
+    default['redisio']['bin_path'] = '/usr/local/bin'
+end
