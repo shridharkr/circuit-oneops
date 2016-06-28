81866b65b14fe861a6a1af572ba71755438c5a17
samawia-moin-confiz <samawiamoin@confiz.com>
2016-06-28 16:36:47 +0500

    Fixed ephemeral storage issue
    Test Plan: n/a
    
    Reviewers: zeeshan.arshad, adnan.khalil, haider.ali, shaffan.chaudhry
    
    Subscribers: omair.sajid
    
    Differential Revision: http://phabricator.confiz.com/D7437

--- components/cookbooks/azure/recipes/del_blobs.rb
+++ components/cookbooks/azure/recipes/del_blobs.rb
@@ -4,6 +4,8 @@ os_disk_blobname = (node['vhd_uri'].split("/").last)
 OOLog.info("Deleting os_disk : #{os_disk_blobname}")
 AzureStorage::AzureDatadisk.delete_disk(node['storage_account'],node['storage_key1'],os_disk_blobname,0)
 
+if node['datadisk_uri'] != nil
 data_disk_blobname = (node['datadisk_uri'].split("/").last)
 OOLog.info("Deleting data_disk : #{data_disk_blobname}")
 AzureStorage::AzureDatadisk.delete_disk(node['storage_account'],node['storage_key1'],data_disk_blobname,0)
+end
