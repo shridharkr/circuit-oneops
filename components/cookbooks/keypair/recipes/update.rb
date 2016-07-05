6e80cb12ad6d48a5e8981c0eba4ea1ba7160d585
Kowsalya Palaniappan - kpalan1 <kowsalya.palaniappan@walmart.com>
2016-07-05 17:00:35 -0500

    Update action will update only sshkeys , doesn't affect the resource group

--- components/cookbooks/keypair/recipes/update.rb
+++ components/cookbooks/keypair/recipes/update.rb
@@ -4,10 +4,6 @@
 #
 
 include_recipe "shared::set_provider"
-  OOLog.debug("node[:provider_class] ==> "+node[:provider_class])
-if node[:provider_class] =~ /azure/
-  return
-else
+
   include_recipe "keypair::delete"
   include_recipe "keypair::add"
-end
