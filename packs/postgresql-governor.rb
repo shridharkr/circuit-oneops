7117a988a6694bfc2f678c8583d03b0008ab96fc
Ning Zhang <nzhang@walmartlabs.com>
2016-07-27 19:42:42 -0700

    PG Governor: bump up default number of compute

--- packs/postgresql-governor.rb
+++ packs/postgresql-governor.rb
@@ -265,11 +265,59 @@ resource "haproxy",
 }
 
 resource "lb",
-  :except => [ 'single' ],
-  :design => false,
-  :attributes => {
+:except => [ 'single' ],
+:design => true,
+:cookbook => "oneops.1.lb",
+:requires => { "constraint" => "1..1", "services" => "compute,lb,dns" },
+:attributes => {
     "listeners" => '["tcp 5432 tcp 5000","tcp 2379 tcp 2379"]',
+},
+:payloads => {
+    'primaryactiveclouds' => {
+        'description' => 'primaryactiveclouds',
+        'definition' => '{
+        "returnObject": false,
+        "returnRelation": false,
+        "relationName": "base.RealizedAs",
+        "direction": "to",
+        "targetClassName": "manifest.Lb",
+        "relations": [
+        { "returnObject": false,
+            "returnRelation": false,
+            "relationName": "manifest.Requires",
+            "direction": "to",
+            "targetClassName": "manifest.Platform",
+            "relations": [
+            { "returnObject": false,
+                "returnRelation": false,
+                "relationAttrs":[{"attributeName":"priority", "condition":"eq", "avalue":"1"},
+                {"attributeName":"adminstatus", "condition":"neq", "avalue":"offline"}],
+                "relationName": "base.Consumes",
+                "direction": "from",
+                "targetClassName": "account.Cloud",
+                "relations": [
+                { "returnObject": true,
+                    "returnRelation": false,
+                    "relationAttrs":[{"attributeName":"service", "condition":"eq", "avalue":"lb"}],
+                    "relationName": "base.Provides",
+                    "direction": "from",
+                    "targetClassName": "cloud.service.Netscaler"
+                }
+                ]
+            }
+            ]
+        }
+        ]
+    }'
 }
+}
+
+#resource "lb",
+#  :except => [ 'single' ],
+#  :design => false,
+#  :attributes => {
+#    "listeners" => '["tcp 5432 tcp 5000","tcp 2379 tcp 2379"]',
+#}
 
 [ 'postgresql-governor' ].each do |from|
   relation "#{from}::depends_on::volume",
@@ -329,6 +377,27 @@ end
   :from_resource => from,
   :to_resource   => 'postgresql-governor',
   :attributes    => { "flex" => false, "min" => 1, "max" => 1 } 
+end
+
+# depends_on
+
+[ 'lb' ].each do |from|
+    relation "#{from}::depends_on::compute",
+    :only => [ 'redundant' ],
+    :relation_name => 'DependsOn',
+    :from_resource => from,
+    :to_resource   => 'compute',
+    :attributes    => { "propagate_to" => 'both', "flex" => true, "current" =>2, "min" => 2, "max" => 10}
+end
+
+# -d name due to pack sync logic uses a map keyed by that name - it doesnt get put into cms
+[ 'lb' ].each do |from|
+    relation "#{from}::depends_on::compute-d",
+    :only => [ '_default' ],
+    :relation_name => 'DependsOn',
+    :from_resource => from,
+    :to_resource   => 'compute',
+    :attributes    => { "flex" => false }
 end
 
 # managed_via
