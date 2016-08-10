a6d59b4225c6106eba45a8463fed6c55b50d8eb8
Phat Huynh <phuynh@walmartlabs.com>
2016-08-10 10:18:31 -0700

    allow user to specify URL of binary package as optional

--- components/cookbooks/jboss/metadata.rb
+++ components/cookbooks/jboss/metadata.rb
@@ -21,6 +21,27 @@ attribute 'version',
     :form => { 'field' => 'select', 'options_for_select' => [['7.1.1','7.1.1'],['7.0.2','7.0.2']] }
   }  
 
+attribute 'jboss_url_enabled',
+  :description => "JBoss URL Enable",
+  :required => "required",
+  :default => 'false',
+  :format => {
+    :help => "",
+    :category => '2.Global',
+    :order => 1,
+    :form => {'field' => 'checkbox'}
+  }
+
+attribute 'jboss_url',
+  :description => "JBoss Download URL",
+  :required => "required",
+  :default => "<Please put in the correct URL for the binary file>",
+  :format => {
+    :help => 'If checkbox is enable, please provide valid download URL for JBoss binary',
+    :category => '2.Global',
+    :order => 2
+  } 
+
 attribute 'jboss_home',
   :description => "JBoss Home",
   :required => "required",
@@ -40,8 +61,6 @@ attribute 'jboss_user',
     :category => '1.Global',
     :order => 3
   } 
-
-
 
 attribute 'java_options',
   :description => "Java Options",
