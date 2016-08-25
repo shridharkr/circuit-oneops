c18c515119aa44e3ef0863a8fc742d4840edef9c
Kevin Griffith <kpgriff@l-hoap3z3g3q-m.homeoffice.wal-mart.com>
2016-08-25 11:03:14 -0500

    removing long description in metadata.rb

--- components/cookbooks/panos/metadata.rb
+++ components/cookbooks/panos/metadata.rb
@@ -3,7 +3,6 @@ maintainer       'OneOps'
 maintainer_email 'support@oneops.com'
 license          'Apache License, Version 2.0'
 description      'Installs/Configures Palo Alto Firewall devices'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 grouping 'default',
