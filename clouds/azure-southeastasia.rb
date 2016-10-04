1a5ef293d7097cbf006a721307de81e6fc214ba0
Oleg Kornev <kornev@gmail.com>
2016-10-03 23:45:37 -0400

    template offerings updated with SPEL criterias instead of EleasticSearch percolator

--- clouds/azure-southeastasia.rb
+++ clouds/azure-southeastasia.rb
@@ -22,168 +22,168 @@ service "azure-southeastasia",
           "description" => "Average Extra Small Azure Standard A0 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.02",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:XS",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='XS'",
           "specification" => "{}"
       },
       "small" => {
           "description" => "Average Small Azure Standard A1 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.06",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:S",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='S'",
           "specification" => "{}"
       },
       "medium" => {
           "description" => "Average medium Azure Standard A2 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.12",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:M",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='M'",
           "specification" => "{}"
       },
       "large" => {
           "description" => "Average large Azure Standard A3 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.24",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:L",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='L'",
           "specification" => "{}"
       },
       "xlarge" => {
           "description" => "Average xlarge Azure Standard A4 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.48",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:XL",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='XL'",
           "specification" => "{}"
       },
       "xxlarge" => {
           "description" => "Average xxlarge Azure Standard A5 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.27",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:XXL",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='XXL'",
           "specification" => "{}"
       },
       "xxxlarge" => {
           "description" => "Average 3xlarge Azure Standard A6 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.54",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:3XL",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='3XL'",
           "specification" => "{}"
       },
       "xxxxlarge" => {
           "description" => "Average 4xlarge Azure Standard A7 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "1.08",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:4XL",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='4XL'",
           "specification" => "{}"
       },
       "smallCPU" => {
           "description" => "Average Small Azure Standard D1 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.098",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:S-CPU",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='S-CPU'",
           "specification" => "{}"
       },
       "mediumCPU" => {
           "description" => "Average medium Azure Standard D2 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.196",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:M-CPU",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='M-CPU'",
           "specification" => "{}"
       },
       "largeCPU" => {
           "description" => "Average large Azure Standard D3 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.392",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:L-CPU",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='L-CPU'",
           "specification" => "{}"
       },
       "xlargeCPU" => {
           "description" => "Average xlarge Azure Standard D4 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.784",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:XL-CPU",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='XL-CPU'",
           "specification" => "{}"
       },
       "8xlargeCPU" => {
           "description" => "Average 8xlarge Azure Standard D11 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.21",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:8XL-CPU",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='8XL-CPU'",
           "specification" => "{}"
       },
       "9xlargeCPU" => {
           "description" => "Average 9xlarge Azure Standard D12 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.42",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:9XL-CPU",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='9XL-CPU'",
           "specification" => "{}"
       },
       "10xlargeCPU" => {
           "description" => "Average 10xlarge Azure Standard D13 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.84",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:10XL-CPU",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='10XL-CPU'",
           "specification" => "{}"
       },
       "11xlargeCPU" => {
           "description" => "Average 11xlarge Azure Standard D14 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "1.68",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:11XL-CPU",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='11XL-CPU'",
           "specification" => "{}"
       },
       "smallMEM" => {
           "description" => "Average small Azure Standard DS1 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.098",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:M-MEM",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='M-MEM'",
           "specification" => "{}"
       },
       "mediumMEM" => {
           "description" => "Average medium Azure Standard DS2 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.196",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:M-MEM",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='M-MEM'",
           "specification" => "{}"
       },
       "largeMEM" => {
           "description" => "Average large Azure Standard DS3 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.392",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:L-MEM",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='L-MEM'",
           "specification" => "{}"
       },
       "xlargeMEM" => {
           "description" => "Average xlarge Azure Standard DS4 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.784",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:XL-MEM",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='XL-MEM'",
           "specification" => "{}"
       },
       "8xlargeMEM" => {
           "description" => "Average 8xlarge Azure Standard DS11 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.21",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:8XL-MEM",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='8XL-MEM'",
           "specification" => "{}"
       },
       "9xlargeMEM" => {
           "description" => "Average 9xlarge Azure Standard DS12 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.42",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:9XL-MEM",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='9XL-MEM'",
           "specification" => "{}"
       },
       "10xlargeMEM" => {
           "description" => "Average 10xlarge Azure Standard DS13 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "0.84",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:10XL-MEM",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='10XL-MEM'",
           "specification" => "{}"
       },
       "11xlargeMEM" => {
           "description" => "Average 11xlarge Azure Standard DS14 cost per Hour",
           "cost_unit" => "USD",
           "cost_rate" => "1.68",
-          "criteria"=> "(ciClassName:bom.*.[1 TO *].Compute OR ciClassName:bom.Compute) AND ciAttributes.size:11XL-MEM",
+          "criteria"=> "(ciClassName matches 'bom.[a-zA-Z0-9.]*.Compute' OR ciClassName=='bom.Compute') AND ciAttributes['size']=='11XL-MEM'",
           "specification" => "{}"
       },
   },
