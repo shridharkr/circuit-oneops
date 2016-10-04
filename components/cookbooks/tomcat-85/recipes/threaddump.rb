5b61fe8d3eb7be2e87961b37015867f2afc9787d
Mary Metzger <mmetzge@L-HOAG5U9G8W-M.local>
2016-10-04 15:29:13 -0500

    threaddump.rb addition

--- components/cookbooks/tomcat-85/recipes/threaddump.rb
+++ components/cookbooks/tomcat-85/recipes/threaddump.rb
@@ -1,8 +1,35 @@
-proc_id= `pgrep -f "org.apache.catalina.startup.Bootstrap"`
+# rubocop:disable LineLength
+###############################################################################
+# Cookbook Name:: tomcat_8-5
+# Recipe:: threaddump
+# Purpose:: This recipe is used to create a threaddump and put in into
+#           the catalina.out file.
+#
+# Copyright 2016, Walmart Stores Incorporated
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+proc_id=`pgrep -f "org.apache.catalina.startup.Bootstrap"`
 proc_id=proc_id.chomp
-tomcatuserid=`ps aux |grep java | grep -v grep  | awk '{print $1}'`
+Chef::Log.debug("proc_id = #{proc_id}")
+
+tomcatuserid=`ps aux | grep java | grep -v grep | awk '{print $1}'`
 tomcatuserid=tomcatuserid.chomp
+Chef::Log.debug("tomcatuserid = #{tomcatuserid}")
+
 thread_dump_cmd="sudo -u  #{tomcatuserid} jstack -l #{proc_id}"
+Chef::Log.debug("thread_dump_cmd = #{thread_dump_cmd}")
+
 puts "Command is #{thread_dump_cmd}"
 if !proc_id.empty?
   ruby_block "jstack_output" do
