1c244fbfff8364d2f25e1c861289ee9634b9f84f
Kire Filipovski <kire.filipovski@gmail.com>
2016-11-07 19:56:27 +0000

    added fqdn and lb to containerized

--- packs/containerized.rb
+++ packs/containerized.rb
@@ -6,7 +6,7 @@ category "Other"
 environment "single", {}
 environment "redundant", {}
 
-entrypoint "replication"
+entrypoint "fqdn"
 
 platform :attributes => {
                 "replace_after_minutes" => 60,
@@ -21,10 +21,287 @@ resource "container",
 resource "replication",
   :cookbook => "oneops.1.replication",
   :design => true,
-  :requires => { "constraint" => "1..1", "services" => "container" }
+  :requires => { "constraint" => "1..1", "services" => "container" },
+  :attributes => {
+    :replicas => '3'
+  }
+
+resource "lb",
+  :design => true,
+  :cookbook => "oneops.1.lb",
+  :requires => { "constraint" => "1..1", "services" => "lb" },
+  :attributes => {
+  },
+  :payloads => {
+    'primaryactiveclouds' => {
+      'description' => 'primaryactiveclouds',
+      'definition' => '{
+         "returnObject": false,
+         "returnRelation": false,
+         "relationName": "base.RealizedAs",
+         "direction": "to",
+         "targetClassName": "manifest.Lb",
+         "relations": [
+           { "returnObject": false,
+             "returnRelation": false,
+             "relationName": "manifest.Requires",
+             "direction": "to",
+             "targetClassName": "manifest.Platform",
+             "relations": [
+               { "returnObject": false,
+                 "returnRelation": false,
+                 "relationAttrs":[{"attributeName":"priority", "condition":"eq", "avalue":"1"},
+                                  {"attributeName":"adminstatus", "condition":"neq", "avalue":"offline"}],
+                 "relationName": "base.Consumes",
+                 "direction": "from",
+                 "targetClassName": "account.Cloud",
+                 "relations": [
+                   { "returnObject": true,
+                     "returnRelation": false,
+                     "relationAttrs":[{"attributeName":"service", "condition":"eq", "avalue":"lb"}],
+                     "relationName": "base.Provides",
+                     "direction": "from",
+                     "targetClassName": "cloud.service.Netscaler"
+                   }
+                 ]
+               }
+             ]
+           }
+         ]
+      }'
+    }
+  }
+
+resource "fqdn",
+  :cookbook => "oneops.1.fqdn",
+  :design => true,
+  :requires => { "constraint" => "1..1", "services" => "dns,*gdns" },
+  :attributes => { "aliases" => '[]' },
+  :payloads => {
+  'environment' => {
+    'description' => 'Environment',
+    'definition' => '{
+       "returnObject": false,
+       "returnRelation": false,
+       "relationName": "base.RealizedAs",
+       "direction": "to",
+       "targetClassName": "manifest.oneops.1.Fqdn",
+       "relations": [
+         { "returnObject": false,
+           "returnRelation": false,
+           "relationName": "manifest.Requires",
+           "direction": "to",
+           "targetClassName": "manifest.Platform",
+           "relations": [
+             { "returnObject": true,
+               "returnRelation": false,
+               "relationName": "manifest.ComposedOf",
+               "direction": "to",
+               "targetClassName": "manifest.Environment"
+             }
+           ]
+         }
+       ]
+    }'
+  },
+  'activeclouds' => {
+    'description' => 'activeclouds',
+    'definition' => '{
+       "returnObject": false,
+       "returnRelation": false,
+       "relationName": "base.RealizedAs",
+       "direction": "to",
+       "targetClassName": "manifest.oneops.1.Fqdn",
+       "relations": [
+         { "returnObject": false,
+           "returnRelation": false,
+           "relationName": "manifest.Requires",
+           "direction": "to",
+           "targetClassName": "manifest.Platform",
+           "relations": [
+             { "returnObject": false,
+               "returnRelation": false,
+               "relationAttrs":[{"attributeName":"priority", "condition":"eq", "avalue":"1"},
+                                {"attributeName":"adminstatus", "condition":"eq", "avalue":"active"}],
+               "relationName": "base.Consumes",
+               "direction": "from",
+               "targetClassName": "account.Cloud",
+               "relations": [
+                 { "returnObject": true,
+                   "returnRelation": false,
+                   "relationAttrs":[{"attributeName":"service", "condition":"eq", "avalue":"gdns"}],
+                   "relationName": "base.Provides",
+                   "direction": "from"
+                 }
+               ]
+             }
+           ]
+         }
+       ]
+    }'
+  },
+  'organization' => {
+    'description' => 'Organization',
+    'definition' => '{
+       "returnObject": false,
+       "returnRelation": false,
+       "relationName": "base.RealizedAs",
+       "direction": "to",
+       "targetClassName": "manifest.oneops.1.Fqdn",
+       "relations": [
+         { "returnObject": false,
+           "returnRelation": false,
+           "relationName": "manifest.Requires",
+           "direction": "to",
+           "targetClassName": "manifest.Platform",
+           "relations": [
+             { "returnObject": false,
+               "returnRelation": false,
+               "relationName": "manifest.ComposedOf",
+               "direction": "to",
+               "targetClassName": "manifest.Environment",
+               "relations": [
+                 { "returnObject": false,
+                   "returnRelation": false,
+                   "relationName": "base.RealizedIn",
+                   "direction": "to",
+                   "targetClassName": "account.Assembly",
+                   "relations": [
+                     { "returnObject": true,
+                       "returnRelation": false,
+                       "relationName": "base.Manages",
+                       "direction": "to",
+                       "targetClassName": "account.Organization"
+                     }
+                   ]
+                 }
+               ]
+             }
+           ]
+         }
+       ]
+    }'
+  },
+  'lb' => {
+    'description' => 'all loadbalancers',
+    'definition' => '{
+       "returnObject": false,
+       "returnRelation": false,
+       "relationName": "bom.DependsOn",
+       "direction": "from",
+       "targetClassName": "bom.oneops.1.Lb",
+       "relations": [
+         { "returnObject": false,
+           "returnRelation": false,
+           "relationName": "base.RealizedAs",
+           "direction": "to",
+           "targetClassName": "manifest.oneops.1.Lb",
+           "relations": [
+             { "returnObject": true,
+               "returnRelation": false,
+               "relationName": "base.RealizedAs",
+               "direction": "from",
+               "targetClassName": "bom.oneops.1.Lb"
+             }
+           ]
+         }
+       ]
+    }'
+  },
+  'remotedns' => {
+       'description' => 'Other clouds dns services',
+       'definition' => '{
+           "returnObject": false,
+           "returnRelation": false,
+           "relationName": "base.RealizedAs",
+           "direction": "to",
+           "targetClassName": "manifest.oneops.1.Fqdn",
+           "relations": [
+             { "returnObject": false,
+               "returnRelation": false,
+               "relationName": "manifest.Requires",
+               "direction": "to",
+               "targetClassName": "manifest.Platform",
+               "relations": [
+                 { "returnObject": false,
+                   "returnRelation": false,
+                   "relationName": "base.Consumes",
+                   "direction": "from",
+                   "targetClassName": "account.Cloud",
+                   "relations": [
+                     { "returnObject": true,
+                       "returnRelation": false,
+                       "relationName": "base.Provides",
+                       "relationAttrs":[{"attributeName":"service", "condition":"eq", "avalue":"dns"}],
+                       "direction": "from"
+                     }
+                   ]
+                 }
+               ]
+             }
+           ]
+      }'
+    },
+   'remotegdns' => {
+       'description' => 'Other clouds gdns services',
+       'definition' => '{
+           "returnObject": false,
+           "returnRelation": false,
+           "relationName": "base.RealizedAs",
+           "direction": "to",
+           "targetClassName": "manifest.oneops.1.Fqdn",
+           "relations": [
+             { "returnObject": false,
+               "returnRelation": false,
+               "relationName": "manifest.Requires",
+               "direction": "to",
+               "targetClassName": "manifest.Platform",
+               "relations": [
+                 { "returnObject": false,
+                   "returnRelation": false,
+                   "relationName": "base.Consumes",
+                   "direction": "from",
+                   "targetClassName": "account.Cloud",
+                   "relations": [
+                     { "returnObject": true,
+                       "returnRelation": false,
+                       "relationName": "base.Provides",
+                       "relationAttrs":[{"attributeName":"service", "condition":"eq", "avalue":"gdns"}],
+                       "direction": "from",
+                     }
+                   ]
+                 }
+               ]
+             }
+           ]
+      }'
+    }
+  }
+
 
 # depends_on
-[ { :from => 'replication',     :to => 'container' } ].each do |link|
+
+[ 'lb' ].each do |from|
+  relation "#{from}::depends_on::replication-redundant",
+    :only => [ 'redundant' ],
+    :design => false,
+    :relation_name => 'DependsOn',
+    :from_resource => from,
+    :to_resource   => 'replication',
+    :attributes    => { "propagate_to" => 'from', "flex" => true, "current" =>3, "min" => 3, "max" => 10}
+end
+
+[ 'lb' ].each do |from|
+  relation "#{from}::depends_on::replication",
+    :except => [ 'redundant' ],
+    :relation_name => 'DependsOn',
+    :from_resource => from,
+    :to_resource   => 'replication',
+    :attributes    => { "propagate_to" => 'from', "flex" => false }
+end
+
+[ { :from => 'replication', :to => 'container' },
+  { :from => 'fqdn',	      :to => 'lb' } ].each do |link|
   relation "#{link[:from]}::depends_on::#{link[:to]}",
     :relation_name => 'DependsOn',
     :from_resource => link[:from],
