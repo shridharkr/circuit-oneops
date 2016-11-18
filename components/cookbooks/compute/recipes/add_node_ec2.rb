a646f40e744fe6351ea9bf5f62cc441ea9bc1905
Kire Filipovski <kire.filipovski@gmail.com>
2016-11-18 09:01:33 -0500

    default user for centos change

--- components/cookbooks/compute/recipes/add_node_ec2.rb
+++ components/cookbooks/compute/recipes/add_node_ec2.rb
@@ -179,7 +179,7 @@ end
 
 if node.ostype =~ /centos/ &&
   node.set["use_initial_user"] = true
-  node.set["initial_user"] = "ec2-user"
+  node.set["initial_user"] = "centos"
 end
 
 puts "***RESULT:private_ip="+server.private_ip_address
