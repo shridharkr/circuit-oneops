553ce99707f28cc9d7a6932ffbe951559456c671
Imaad Ghouri <ighouri@walmartlabs.com>
2016-12-07 16:55:19 -0800

    removing swift package installation from object store

--- components/cookbooks/swift/recipes/delete_objectstore.rb
+++ components/cookbooks/swift/recipes/delete_objectstore.rb
@@ -1,11 +1,4 @@
-platform = node[:swift][:platform]
-platform['swift_client_packages'].each do |pkg|
-  package pkg do
-    options platform['override_options']
 
-    action :purge
-  end
-end
 template node[:swift][:homepath] + "/openrc" do
   source 'openrc.erb'
   mode 00600
